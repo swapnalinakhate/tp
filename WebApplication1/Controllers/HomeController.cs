1d59b41a581b358d731f8c45dadabc48de22d83f
swap <swap@gmail.com>
2022-04-01 18:23:20 +0530

    second commit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index1()
         {
             return View();
         }
